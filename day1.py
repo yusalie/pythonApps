aafb92da8d83dac11c98a57bbdc2e5fc235a9903
yusalie <71071393+yusalie@users.noreply.github.com>
2020-09-21 11:04:02 +0200

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,9 @@
+print("hello world")
+
+class_name = input("class number name\n")
+
+print("I was coded in " + class_name)
+
+class_students = input("number of students\n")
+
+print("there are " + class_students + " students in " + class_name)
