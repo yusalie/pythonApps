03dee36ca6fb6035ff64e42952c5aa707d3cb347
yusalie <71071393+yusalie@users.noreply.github.com>
2020-09-21 11:45:18 +0200

    Add files via upload

---
+++ day1_task2.py
@@ -0,0 +1,10 @@
+#Asks raduis
+radius = int(input("what is radius\n"))
+#Value of pi
+pi = 3.1415
+
+#Calculates circumference
+circumference = 2 * radius * pi
+
+#Outputs answer
+print("the circumference is " , circumference)
---
+++ day_task1.py
@@ -0,0 +1,5 @@
+#Asks name
+user_Name = input("what is your name?\n")
+
+#Outputs answer
+print("Hello " + user_Name + " how are you?")
